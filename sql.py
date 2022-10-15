e0714598d8992d2137d0e20a20240d2eea27e035
lcc-666 <2502247952@qq.com>
2022-10-15 21:18:37 +0800

    数据

--- sql.py
+++ sql.py
@@ -4,12 +4,16 @@ import pymysql
 # sql语句作为参数传入
 def getsql(sql):
     global result
+    # conn = pymysql.connect(
+    #     host="127.0.0.1",
+    #     user="root", password="123456",
+    #     database="data",
+    #     charset="gbk")
     conn = pymysql.connect(
-        host="127.0.0.1",
-        user="root", password="123456",
+        host="www.chaogezuishuai.top",
+        user="chao", password="CHAOGE",
         database="data",
         charset="gbk")
-
     cursor = conn.cursor()
     if isinstance(sql, str):
         cursor.execute(sql)
